a77c8694f7901fcc51bf631cbbad09141289dc65
Evaldas Melianas <101465099+psysheep@users.noreply.github.com>
2023-01-21 10:53:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ def erase():
     text_widget.insert(END, filemod.erase())
 
 
+def find_adv():
+    symbol = str(find_adv_entry1.get())
+    length = int(find_adv_entry2.get())
+    text_widget.delete(1.0, END)
+    text_widget.insert(END, filemod.finder_adv(symbol, length))
+
+
 def find():
     symbol = find_entry.get()
     text_widget.delete(1.0, END)
@@ -41,22 +48,27 @@ text_widget.pack(side=BOTTOM)
 create_button = Button(mainframe, text="Create File", command=create_file)
 create_button.pack(side=RIGHT)
 
-add_entry = Entry(mainframe)
-add_entry.pack(side=LEFT)
-
-add_button = Button(mainframe, text="Add Line", command=add_line)
-add_button.pack(side=LEFT)
+erase_button = Button(mainframe, text="Erase File", command=erase)
+erase_button.pack(side=RIGHT)
 
 read_button = Button(mainframe, text="Read File", command=read)
 read_button.pack(side=RIGHT)
 
-erase_button = Button(mainframe, text="Erase File", command=erase)
-erase_button.pack(side=RIGHT)
+add_entry = Entry(mainframe)
+add_entry.pack(side=BOTTOM)
+add_button = Button(mainframe, text="Add Line", command=add_line)
+add_button.pack(side=BOTTOM)
 
 find_entry = Entry(mainframe)
-find_entry.pack(side=LEFT)
+find_entry.pack(side=BOTTOM)
+find_button = Button(mainframe, text="Find symbol", command=find)
+find_button.pack(side=BOTTOM)
 
-find_button = Button(mainframe, text="Find", command=find)
-find_button.pack(side=LEFT)
+find_adv_button = Button(mainframe, text="Find #1symbol and #2length", command=find_adv)
+find_adv_button.pack(side=TOP)
+find_adv_entry1 = Entry(mainframe)
+find_adv_entry1.pack(side=TOP)
+find_adv_entry2 = Entry(mainframe)
+find_adv_entry2.pack(side=TOP)
 
 mainframe.mainloop()
